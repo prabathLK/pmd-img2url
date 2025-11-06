05b8cb2f6911492b8bcb704aabb0a261eb969024
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2025-11-06 12:57:37 +0530

    Create Image.js

---
+++ models/Image.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const ImageSchema = new mongoose.Schema({
+  fileKey: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  originalName: {
+    type: String,
+    required: true,
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now,
+  },
+  lastAccessedDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model('Image', ImageSchema);
